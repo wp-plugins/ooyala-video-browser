94d7a60e5edb8dea12ceacc78ee561fd4d6f723f
PeteMall <PeteMall@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-24 14:47:05 +0000

    Bump to 1.6
    git-svn-id: https://plugins.svn.wordpress.org/ooyala-video-browser/trunk@658048 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ooyala.php
+++ ooyala.php
@@ -3,9 +3,9 @@
 Plugin Name: Ooyala Video
 Plugin URI: http://www.ooyala.com/wordpressplugin/
 Description: Easy Embedding of Ooyala Videos based off an Ooyala Account as defined in the <a href="options-general.php?page=ooyala-options"> plugin settings</a>.
-Version: 1.5
+Version: 1.6
 License: GPL
-Author: David Searle
+Author: Ooyala
 
 Contact mail: wordpress@ooyala.com
 */
