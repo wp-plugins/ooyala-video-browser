7a3a1164aaeec0d82b275460d18d9ed87df3b35f
PeteMall <PeteMall@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-06 19:17:04 +0000

    Tagging 1.7.4
    git-svn-id: https://plugins.svn.wordpress.org/ooyala-video-browser/tags/1.7.4@909186 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ooyala.php
+++ ooyala.php
@@ -3,7 +3,7 @@
 Plugin Name: Ooyala Video
 Plugin URI: http://www.ooyala.com/wordpressplugin/
 Description: Easy Embedding of Ooyala Videos based off an Ooyala Account as defined in the <a href="options-general.php?page=ooyala-options"> plugin settings</a>.
-Version: 1.7.2
+Version: 1.7.4
 License: GPL
 Author: Ooyala
 
